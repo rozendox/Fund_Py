35cf21792537aa28365996921054bcf85b94aa1b
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-01-05 05:52:19 -0300

    Update and rename ganhos_por_hora to ganhos_por_hora.py

--- ganhos_por_hora.py
+++ ganhos_por_hora.py
@@ -1,10 +1,11 @@
 """
 
+@rozendox_
+
 Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 Calcule e mostre o total do seu salário no referido mês.
 
 um trabalhador que trabalha de segunda a sexta
-@rozendo_dev
 """
 
 
